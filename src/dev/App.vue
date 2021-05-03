728823016a7c50ea9b60603d28b7af28b73d8b0a
maguastupaguas <mauricioaznar94@gmail.com>
2021-05-03 18:05:26 -0500

    Updates

---
+++ src/dev/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col>
+          <maguas-text-field v-model="text" name="name" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import MaguasTextField from "@/components/forms/MaguasTextField";
+export default {
+  components: { MaguasTextField },
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
