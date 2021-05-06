d9472810ad3b15a8d314ed3ca85e1148c02b2e79
mauricioaznar <mauricioaznar94@gmail.com>
2021-05-05 20:59:10 -0500

    Timelines

--- dev/plugins/maguas-vuetify2.ts
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-import MaguasVuetify2 from "@/main"; /* point to /src/plugin.js for dev purpose
-in production replace with: import VuetifyExtra from '@menteora/vuetify-extra' and
-import '@menteora/vuetify-extra/dist/menteora.css' */
-
-Vue.use(MaguasVuetify2);
